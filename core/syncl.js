5e70eae9e1fede1b1d787af0c7e96c59621f13a7
zhouzhichuang <zhouzhichuang@meizu.com>
2016-06-02 21:04:15 +0800

    修改syncl

--- core/syncl.js
+++ core/syncl.js
@@ -1,31 +1,19 @@
-var path = require('path'),
-    syspath = require('path'),
+var syspath = require('path'),
     globToRegExp = require('glob-to-regexp'),
     walk = require('walk'),
-    fs = require('fs');
+    fs = require('fs'),
+    inquirer = require('inquirer');
 
-var fse = require('fs-extra');
-
-var Util = require('./util');
-
-var config = require('../config');
-
-var testDir = 'test';
-
-var syncTestErrN = 0,
-    syncTestSucN = 0;
-
-var parentPath = process.cwd().split('/').slice(0, -1).join('/');
-
-var CONFIG_FILE_NAME = 'i18n.config.json';
+var parentPath = process.cwd().split('/').slice(0, -1).join('/'); // **/source
+var CONFIG_FILE_NAME = 'i18n.config.json'; 
 var COMMON_CONGIG_FILE_PATH = '../../lib/mz-i18n-conf.json';
-
 var IGNORE_SOURCE_DIRS = ['i18n-php-server'];
 
 var R = require('fw-ramda');
 
+// 从 list数组 中排除 items 数组中的项
 var removeItems = function(list, items){
-    var newList = Array.prototype.slice(list);
+    var newList = Array.prototype.slice.call(list);
     items.forEach(function(item){
         var i = newList.indexOf(item);
         newList.splice(i, i);
@@ -33,95 +21,52 @@ var removeItems = function(list, items){
     return newList;
 };
 
-var copy = function(source, targets, p){
-    var stat = fs.statSync(source);
-    if( stat.isDirectory() ){
-        fs.readdir(source, function(err, paths){
-            paths.forEach(function(path){
-                if( path !== '.' && path !== '..' ){
-                    copy(source + '/' + path, targets, syspath.join(p || '', path));
-                }
-            });
-        });
-    } else if( stat.isFile() ){
-        fs.readFile(source, function(err, data){
-            if( err ){
-                throw err;
-            }
-            targets.forEach(function(target){
 
-                var tpath = syspath.join(target.path, p);
-                if( target.excludeRegs.every(function(e){
-                    return !e.test(tpath);
-                }) ){
-                    var ppath = R.dropLast(1, tpath.split('/')).join('/');
-                    fse.ensureDir(ppath, function(err){
-                        if( err ){
-                            throw err;
-                        }
-                        fs.writeFile(tpath, data, function(err){
-                            if( err ){
-                                throw err;
-                            }
-                        });
-                    });
-                }
-            });
-            
-        });
-    }
-};
-
+// 同步命令入口
 var Syncl = function(args) {
-
     if( !/source$/.test(parentPath) ){
         fis.log.warn('请进入语言路径进行操作!');
         process.exit(0);
     }
     
     fis.log.info('开始同步了骚年！');
-    console.log();
     
-    var i18nConfig;
-    var commonConfig;
+    // 配置读取
+    var i18nConfig; // 当前国家目录下的同步配置
+    var commonConfig; // 公共同步配置
     
     try {
         i18nConfig = JSON.parse(fs.readFileSync(CONFIG_FILE_NAME));
         commonConfig = JSON.parse(fs.readFileSync(COMMON_CONGIG_FILE_PATH));
     } catch (error) {
         console.log('读取配置文件失败,请检查', CONFIG_FILE_NAME, COMMON_CONGIG_FILE_PATH);
+        console.log(error);
         process.exit();
     }
     
-    var currentLanVersion = R.last(path.resolve('.').split('/')),
-        currentPath = path.resolve('.');
+    // 当前国家
+    var currentLanVersion = R.last(syspath.resolve('.').split('/')),
+        currentPath = syspath.resolve('.');
 
-    //var productsInclude = commonConfig.product;
-
+    // 将当前国家添加到排除规则之外，避免自己同步自己
     if( i18nConfig.syncDir.exclude.indexOf(currentLanVersion) < 0 ){
         i18nConfig.syncDir.exclude.push(currentLanVersion);
     }
     
-    var paths = fs.readdirSync('..').filter(function(path){
-        return fs.lstatSync('../' + path).isDirectory();
-    }).filter(function(path){
-        return IGNORE_SOURCE_DIRS.indexOf(path) < 0;
+    // 所有国家
+    var countrys = fs.readdirSync('..').filter(function(path){
+        return fs.lstatSync('../' + path).isDirectory() && IGNORE_SOURCE_DIRS.indexOf(path) < 0;
     });
 
-    
-
-    var products = commonConfig.product;
-
-    var productsNames = Object.keys(products);
-    
-    var specVersions = Object.keys(i18nConfig.specRule);
+    var products = commonConfig.product; // 所有产品的匹配规则
+    var productsNames = Object.keys(products); // 所有产品名
+    var specVersions = Object.keys(i18nConfig.specRule); // 所有配置了同步规则的国家
     
     if( !specVersions ){
         fis.log.error('');
     }
     
-    var includeReg = globToRegExp(i18nConfig.syncDir.include);
-
+    // 各个国家同步配置
     var specRules = Object.keys(i18nConfig.specRule).reduce(function(res, keys){
         if( keys.indexOf(',') ){
             keys.split(',').forEach(function(key){
@@ -132,204 +77,177 @@ var Syncl = function(args) {
         }
         return res;
     }, {});
-    
-    if( args.length === 0 ){
-        // 默认的情况
 
-        var spaths = paths.filter(function(path){
-            return includeReg.test(path);
-        }).filter(function(path){
-            return i18nConfig.syncDir.exclude.indexOf(path) < 0;
-        }).map(function(path){
-            var exclude;
-            
-            if( i18nConfig.specRule[path] ){
-                var spec = i18nConfig.specRule[path];
-                var commonRule = commonConfig.rule[spec['useCommonRule']];
-                
-                if( !commonConfig ){
-                    exclude = commonConfig.rule['default'].exclude;
-                } else {
-                    exclude = R.concat(commonRule.exclude, spec.exclude);
-                }
 
-                var vproducts = i18nConfig.specRule[path].products;
-                if( !vproducts ){
-                    fis.log.error(path + ' 没有指定同步的产品!');
-                    process.exit(0);
-                } else {
-                    var excludeProducts = removeItems(productsNames, vproducts);
-                    
-                    excludeProducts.forEach(function(p){
-                        products[p].forEach(function(e) {
-                            exclude.push(e);
-                        });
-                    });
-                }
+    var sCountrys,
+        country,
+        product,
+        isInit;
 
-                var excludeRegs = exclude.map(function(e){
-                    return globToRegExp(syspath.join(syspath.resolve('.'), e));
-                });
-
-                fis.log.info('复制 ', path, ' ING.....');
-                fis.util.copy(syspath.resolve('.'), syspath.join('../', path), null, excludeRegs);
-            }
-
-        });        
-        
-    } else if( args.length === 1 ){
-        // 只表达一个参数 可能是 国家 可能是 products
-        
-        var command = args[0];
-
-        if( paths.indexOf(command) >= 0 ) {
-            var exclude;
-            
-            if( i18nConfig.specRule[command] ){
-                var spec = i18nConfig.specRule[command];
-                var commonRule = commonConfig.rule[spec['useCommonRule']];
-                
-                if( !commonConfig ){
-                    exclude = commonConfig.rule['default'].exclude;
-                } else {
-                    exclude = R.concat(commonRule.exclude, spec.exclude);
-                }
-                
-                var vproducts = i18nConfig.specRule[command].products;
-                if( !vproducts ){
-                    fis.log.error(path + ' 没有指定同步的产品!');
-                    process.exit(0);
-                } else {
-                    var excludeProducts = removeItems(productsNames, vproducts);
-                    excludeProducts.forEach(function(p){
-                        products[p].forEach(function(e) {
-                            exclude.push(e);
-                        });
-                    });
-                }
-                
-                var excludeRegs = exclude.map(function(e){
-                    return globToRegExp(syspath.join(currentPath, e));
-                });
-
-                fis.log.info('复制 ', command, ' ING.....');
-                return fis.util.copy(syspath.resolve('.'), syspath.join('../', command), null, excludeRegs);
-                fis.log.info('Done!');
-            } else {
-                return fis.log.warn('你没有在 i18n.config.json 配置好', command, '噢');
-            }
-            
+    // 参数分配
+    args.forEach(function(arg) {
+        if (countrys.indexOf(arg) !== -1) {
+            country = arg;
+        } else if (productsNames.indexOf(arg) !== -1) {
+            product = arg;
+        } else if (arg.toLowerCase() === 'init') {
+            isInit = true;
         }
-        
-        if( Object.keys(products).indexOf(command) >= 0 ){
-            
-            paths.filter(function(path){
-                return includeReg.test(path);
-            }).filter(function(path){
-                return i18nConfig.syncDir.exclude.indexOf(path) < 0;
-            }).map(function(path){
+    });
+    isInit = product ? isInit : false;
 
-                if( specVersions.indexOf(path) < 0 ){
-                    return; 
-                }
-                
-                var exclude;
-                
-                if( i18nConfig.specRule[path] ){
-                    var spec = i18nConfig.specRule[path];
-                    var commonRule = commonConfig.rule[spec['useCommonRule']];
-                    if( !commonConfig ){
-                        fis.log.error('你用哪个 CommonRule ?');
-                        process.exit();
-                    }
-                    exclude = R.concat(commonRule.exclude, spec.exclude);
-                } else {
-                    exclude = commonConfig.rule['default'].exclude;
-                }
-                
-                var excludeRegs = exclude.map(function(e) {
-                    return globToRegExp(syspath.join(syspath.resolve('.'), e));
-                });
-                
-                var includeRegs = products[command].map(function(e) {
-                    return globToRegExp(syspath.join(syspath.resolve('.'), e));
-                });
-                fis.log.info('复制 ', path, '的', command, ' ING.....');
-                return fis.util.copy(syspath.resolve('.'), syspath.join('../', path), includeRegs, excludeRegs);
-            });
-            fis.log.info('Done!');
+    sCountrys = countrys.filter(function(country){
+        return (i18nConfig.syncDir.exclude.indexOf(country) < 0 && 
+                globToRegExp(i18nConfig.syncDir.include).test(country));
+    });
+    if (country) { 
+        if (sCountrys.indexOf(country) === -1) {
+            fis.log.error('在 ' + CONFIG_FILE_NAME + ' 中，没有匹配到该国家的配置，请检查');
+            return;
         }
-
-        fis.log.warn('What are you talking about?');
-        
-    } else if( args.length === 2 ){
-        // 两个参数 lang + product
-        
-        var language = args[0],
-            productName = args[1];
-
-        if( paths.indexOf(language) < 0 || specVersions.indexOf(language) < 0 ){
-            fis.log.warn('没有', language, '啊。');
-            return process.exit(0);
-        }
-
-        if( productName.indexOf(productName) < 0 ){
-            fis.log.warn('你没有配置', productName, '啊。');
-            return process.exit(0);
-        }
-
-        
-        var includeRegs = products[productName].map(function(e){
-            return globToRegExp(syspath.join(syspath.resolve('.'), e));
-        });
-
-        fis.log.info('复制 ', language, '的', productName, ' ING.....');
-        fis.util.copy(syspath.resolve('.'), syspath.join('../', language), includeRegs, null);
-    } else {
-        fis.log.error('参数不合法');
+        sCountrys = [country]
     }
-    
-};
 
-var processX = function(){
-    var conti = true;
-    var printProgress = function(){
-        process.stdout.write('.');
-        setTimeout(function(){
-            if ( conti ) {
-                printProgress();  
+    inquirer.prompt([{
+
+        type: 'confirm',
+        name: 'ok',
+        message: (country ? '\n' + '你要同步的国家是: ' + sCountrys.join(' ') + '\n' : '')
+                + (product ? '你要同步的产品是: ' + product + '\n' : '')
+                + (product && isInit ? '这是第一次为该国家创建此产品。' + '\n': '')
+                + ('确定吗？')
+
+    }], function(answers) {
+        if (answers.ok) {
+            syncGo();
+        } else {
+            fis.log.info('债见');
+            return;
+        }
+    }); 
+
+    function syncGo() {
+        sCountrys.forEach(function(country) {
+            if(!product){
+
+                // 同步国家
+                syncCountry(country);
+                return;
+
+            } else {
+                // 同步国家 下的该产品 product
+                syncCountryProducts(country, product, isInit);
             }
-        }, 5);
-    };
-    printProgress();
-    return function(){
-        conti = false;
-    };
+        });
+    } 
+
+
+    // 获取某一个国家的排除规则
+    function getExcludeOfCountry(country) {
+        var excludesRegs;
+        var spec = specRules[country];
+        var commonRule = commonConfig.rule[spec['useCommonRule']];
+        
+        // 排除
+        excludesRegs =  R.concat(
+                            commonConfig.rule['default'].exclude, 
+                            (commonRule ? commonRule : []),
+                            (spec.exclude || [])
+                        );
+        excludesRegs = excludesRegs.map(function(e){
+            return syspath.join(currentPath, e);
+        });  
+        return excludesRegs;      
+    }
+
+    // 同步某一个国家
+    function syncCountry(country) {
+        if( !specRules[country] ){
+            return fis.log.warn('你没有在 i18n.config.json 配置好 ', country, ' 国家噢');
+        }
+
+        var excludesRegs = getExcludeOfCountry(country);
+        var spec = specRules[country];
+
+        // 排除指定产品之外的其他产品
+        var excludeProducts = removeItems(productsNames, spec.products);
+        
+        excludeProducts.forEach(function(p){
+            products[p].forEach(function(e) {
+                excludesRegs.push(syspath.join(currentPath, e));
+            });
+        });
+         console.log(excludesRegs); //return;
+
+        fis.log.info('同步 ', country, ' ING...');
+        fis.util.copy(syspath.resolve('.'), syspath.join('../', country), null, excludesRegs);
+        removeEmptyDir(syspath.join('../', country));
+        fis.log.info('同步 ', country, ' DONE...');
+    }
+
+    // 同步某个国家下面的产品，isInit 为真代表第一次同步
+    // （既忽略公共排除规则，图片，php也复制）
+    function syncCountryProducts(country, productName, isInit) {
+        if( !specRules[country] ){
+            return fis.log.warn('你没有在 i18n.config.json 配置好 ', country, ' 国家哦');
+        }
+        if( !products[productName] ){
+            return fis.log.warn('你没有在 mz-i18n-conf.json 配置好 ', productName, ' 产品哦');
+        }
+        if( specRules[country].products.indexOf(productName) === -1 ){
+            return fis.log.warn('你没有在 i18n.config.json 配置的 ', country, ' 国家中定制 ', productName, ' 产品哦');
+        }
+
+        var excludesRegs;
+        var spec = specRules[country];
+
+        // 排除
+        excludesRegs = isInit ? []: getExcludeOfCountry(country);
+        
+        var includeRegs = products[productName];
+
+        console.log(excludesRegs); //return; mz i18n syncl id pro6
+
+        fis.log.info('同步 ', productName , ' 到 ', country , ' ING...');
+        fis.log.info('你指定了 init 参数，将为你第一次创建此产品');
+        fis.util.copy(syspath.resolve('.'), syspath.join('../', country), includeRegs, excludesRegs);
+        removeEmptyDir(syspath.join('../', country));
+        fis.log.info('同步 ', productName , ' 到 ', country , ' DONE...');
+    }
+
 };
 
-var copyTest= function(originPath, namespace, cb){
-    var walker = walk.walk(originPath + '/' + testDir, { followLinks: false });
-    var reg = new RegExp('=>(\\s\'\/)(.*\/)(\'\.)', 'g');
-    var currentPath = process.cwd();
-    walker.on("file", function(root, fileStat, next){
-        fs.readFile(path.resolve(root, fileStat.name), 'utf-8', function (err, data) {
-            syncTestSucN++;
-            var relativePath = root.split(originPath)[1];
-            var currentFile = '.' + relativePath + '/' + fileStat.name;
-            var newContent = data.replace(reg, function($0 ,$1, $2, $3){
-                return $1 + namespace + '/' + $2 + $3;
-            });
-            fs.writeFile(currentFile, newContent, function(){});
-            next();
-        });
+
+// 删除空目录
+function removeEmptyDir(dir) {
+    var fs = require('fs'),
+        path = require('path'),
+        fsUtils = require('nodejs-fs-utils'),
+        fileList = fs.readdirSync(dir),
+        empty = true;
+
+    var currentPath,
+        status;
+
+
+    fileList.forEach(function(fileName) {
+        currentPath = path.join(dir, fileName);
+
+        if (currentPath.indexOf('.DS_Store') >= 0) {
+            fs.unlinkSync(currentPath);
+        } else if (currentPath.indexOf('.svn') >= 0) {
+            fsUtils.rmdirsSync(currentPath);
+        } else if (fs.statSync(currentPath).isDirectory()) {
+            empty = removeEmptyDir(currentPath) && empty;
+        } else {
+            empty = false;
+        }
     });
-    walker.on("errors", function(root, nodeStatsArray, next){
-        syncTestErrN++;
-        next();
-    }); // plural
-    walker.on("end", function(){
-        cb();
-    });
-};
+
+    if (empty) {
+        fs.rmdirSync(dir);
+    }
+    return empty;
+}
 
 module.exports = Syncl;
